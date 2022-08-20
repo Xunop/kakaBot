efc5011ae762f8b2f98f4e9cbc5c7da11d4a2069
Xunop <1919581623@qq.com>
2022-08-21 01:02:29 +0800

    fix:删除一些不必要文件

--- src/test/java/com/mybot/kakaBot/BotDemoApplicationTests.java
+++ src/test/java/com/mybot/kakaBot/BotDemoApplicationTests.java
@@ -3,18 +3,8 @@ package com.mybot.kakaBot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.ArrayList;
-
 @SpringBootTest
 class BotDemoApplicationTests {
 
-    @Test
-    void contextLoads() throws InterruptedException {
-        ArrayList<String> strings = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            strings.add("==" + i);
-        }
-        System.out.println(strings.toString());
-    }
 
 }
